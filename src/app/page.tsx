e8727c6f8255076aa0b0b76de58893152b3deea6
elmanzeynalov <elman.zeynalov@gmail.com>
2025-03-14 13:27:21 +0400

    create home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,20 @@
-
+import {Poppins} from 'next/font/google';
+import { Button } from "@/components/ui/button"
+import { LoginButton } from '@/components/auth/login-button';
 export default function Home() {
 	return (
-		<p className='font-semibold text-amber-800'>home</p>
+		<main className='flex h-full min-h-screen flex-col items-center justify-center bg-amber-900 p-24'>
+			<div className='space-y-6 text-center'>
+				<h1 className='text-6xl font-semibold text-white drop-shadow-md'>Authentication</h1>
+				<p className='text-white text-lg'>Authentication service</p>
+			</div>
+			<div>
+				<LoginButton>
+				<Button variant='secondary' size={'lg'}>
+					Sing In
+				</Button>
+				</LoginButton>
+			</div>
+		</main>
 	);
 }
