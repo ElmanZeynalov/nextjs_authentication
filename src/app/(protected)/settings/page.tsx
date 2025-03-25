abeb9d26c943de9379480bbfff8326d5413e7a64
elmanzeynalov <elman.zeynalov@gmail.com>
2025-03-25 16:45:01 +0400

    sing out button

--- src/app/(protected)/settings/page.tsx
+++ src/app/(protected)/settings/page.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
-import { auth } from '../../../../auth';
+import { auth,signOut } from '../../../../auth';
+import { Button } from '@/components/ui/button';
+
 
 async function Page() {
 	const session = await auth();
 
 	console.log(JSON.stringify(session));
-	return <div>settings</div>;
+	return <div>{JSON.stringify(session)}
+
+		<form onSubmit={
+			async ()=>{
+				'use server'
+				await signOut();
+			}
+		}>
+			<Button type="submit">sing out</Button>
+		</form>
+
+	</div>;
 }
 
 export default Page;
